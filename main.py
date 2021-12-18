416449f81baa5ac1f64bd955d2f103c5364b91a7
Eta06 <emrtnhalim@gmail.com>
2021-12-18 20:22:41 +0300

    Comments added.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from captcha.image import ImageCaptcha
 #   We define the dimensions of the image we want to be created.
 image = ImageCaptcha(width= 280, height= 90)
 #   We define the text that will be created on the captcha.
-captcha_text = 'Sude'
+captcha_text = 'Love'
 
 data = image.generate(captcha_text)
-#   We define the
+#   We define the name of the created png file
 image.write(captcha_text, 'CAPTCHA.png')
